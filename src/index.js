92a55f3de028ea17cf83883e480b8619f9e4b558
Sasha Fu <mariafu0801@gmail.com>
2021-03-25 22:35:27 +0800

    connect props

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import faker from "faker";
 import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 09:36PM"
+        post="Hello World!"
+        avatar={faker.image.animals()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Today at 05:02PM"
+        post="Who you are?"
+        avatar={faker.image.people()}
+      />
+      <CommentDetail
+        author="Kevin"
+        timeAgo="Yesterday at 03:25AM"
+        post="Front-End developer!!"
+        avatar={faker.image.image()}
+      />
     </div>
   );
 };
