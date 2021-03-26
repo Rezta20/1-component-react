cea2c7a49caa8e14fe1358c44ab57912ff1e4f07
Sasha Fu <mariafu0801@gmail.com>
2021-03-26 14:25:22 +0800

    connent props children in ApprovalCard

--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
-import CommentDetail from "./CommentDetail";
 
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 09:36PM"
-        post="Hello World!"
-        avatar={faker.image.animals()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Today at 05:02PM"
-        post="Who you are?"
-        avatar={faker.image.people()}
-      />
-      <CommentDetail
-        author="Kevin"
-        timeAgo="Yesterday at 03:25AM"
-        post="Front-End developer!!"
-        avatar={faker.image.image()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 09:36PM"
+          post="Hello World!"
+          avatar={faker.image.animals()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Today at 05:02PM"
+          post="Who you are?"
+          avatar={faker.image.people()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Kevin"
+          timeAgo="Yesterday at 03:25AM"
+          post="Front-End developer!!"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
